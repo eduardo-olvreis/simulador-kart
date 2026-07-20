331460079d83e957cd03c553d08f8a14ee85b7a6
Eduardo <eduardo.olvreis@gmail.com>
2026-07-19 23:40:52 -0300

    feat: entidade do piloto e do veiculo

---
+++ KartSimulator/Entities/Piloto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.Entities
+{
+    public class Piloto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Nome' não pode ser vazio.")]
+        [MaxLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Range(18, 120, ErrorMessage = "A idade deve estar entre o intervalo de 18 a 120.")]
+        public int Idade { get; set; }
+
+        public int CorridasVencidas { get; set; } = 0;
+
+        [Range(0, 100, ErrorMessage = "A Habilidade de Curva deve ser entre o intervalo de 0 a 10.")]
+        public int HabilidadeCurvas { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A Consistência deve estar entre o intervalo de 0 a 10.")]
+        public int Consistencia { get; set; }
+
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+    }
+}
---
+++ KartSimulator/Entities/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.Entities
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Modelo' não pode ser vazio.")]
+        [MaxLength(60, ErrorMessage = "Modelo não pode ultrapassar 60 caracteres.")]
+        public string Modelo { get; set; }
+
+        [Range(45, 70, ErrorMessage = "A Velocidade Máxima deve ser entre 45 e 70.")]
+        public int VelocidadeMaxima { get; set; }
+        
+        public Piloto? Piloto { get; set; }
+    }
+}
