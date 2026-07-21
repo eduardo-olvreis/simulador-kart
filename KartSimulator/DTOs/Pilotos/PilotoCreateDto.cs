ac7e1c1d9e424af4cf7ccfdf51b0d31b18165732
Eduardo <eduardo.olvreis@gmail.com>
2026-07-21 16:59:35 -0300

    feat: PilotoDTO (falta fazer o relacionamento com o veiculoDTO)

---
+++ KartSimulator/DTOs/Pilotos/PilotoCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.DTOs.Pilotos
+{
+    public class PilotoCreateDto
+    {
+        [Required(ErrorMessage = "Campo 'Nome' não pode ser nulo.")]
+        [MaxLength(100, ErrorMessage = "Campo 'Nome' não pode ser maior que 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Range(18, 120, ErrorMessage = "Campo 'Idade' deve estar entre o intervalo de 18 a 120.")]
+        public int Idade { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Campo 'Habilidade Curvas' deve estar entre o intervalo de 0 a 10.")]
+        public int HabilidadeCurvas {  get; set; }
+
+        [Range(0, 10, ErrorMessage = "Campo 'Consistência' deve estar entre o intervalo de 0 a 10.")]
+        public int Consistencia { get; set; }
+    }
+}
---
+++ KartSimulator/DTOs/Pilotos/PilotoResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.DTOs.Pilotos
+{
+    public class PilotoResponseDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int CorridasVencidas { get; set; }
+        public int HabilidadeCurvas { get; set; }
+        public int Consistencia { get; set; }
+    }
+}
