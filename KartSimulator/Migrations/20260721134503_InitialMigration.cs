035d817a5950fbeebac14d17ad8df4dda2fc0e64
Eduardo <eduardo.olvreis@gmail.com>
2026-07-21 10:47:28 -0300

    feat: builder dbContext na Program e Initial Migration

---
+++ KartSimulator/Migrations/20260721134503_InitialMigration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KartSimulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    VelocidadeMaxima = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pilotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    CorridasVencidas = table.Column<int>(type: "int", nullable: false),
+                    HabilidadeCurvas = table.Column<int>(type: "int", nullable: false),
+                    Consistencia = table.Column<int>(type: "int", nullable: false),
+                    VeiculoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pilotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pilotos_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pilotos_VeiculoId",
+                table: "Pilotos",
+                column: "VeiculoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pilotos");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
