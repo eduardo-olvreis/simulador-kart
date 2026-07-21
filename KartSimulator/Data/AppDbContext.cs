748d1c9a589c923645ccf4961fb5bbbd89aa8e9a
Eduardo <eduardo.olvreis@gmail.com>
2026-07-21 10:36:15 -0300

    feat: DbContext e ConnectionStrings no appsettings

---
+++ KartSimulator/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using KartSimulator.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KartSimulator.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
