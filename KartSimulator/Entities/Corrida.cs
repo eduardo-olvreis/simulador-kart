71416bcec4e1f77b5fb0204a584477bedf8683e0
Eduardo <eduardo.olvreis@gmail.com>
2026-07-23 20:44:27 -0300

    feat: criação da tabela Corrida

---
+++ KartSimulator/Entities/Corrida.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.Entities
+{
+    public class Corrida
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Nome da Pista' não pode ser nulo.")]
+        [MaxLength(60, ErrorMessage = "Campo 'Nome da Pista' não pode ter mais de 60 caracteres.")]
+        public string NomePista { get; set; }
+
+        [Range(10,50, ErrorMessage = "Campo 'Numero de Voltas' deve estar entre o intervalo de 10 a 50.")]
+        public int NumeroVoltas { get; set; }
+
+        public DateTime DataCorrida { get; set; } = DateTime.UtcNow;
+    }
+}
