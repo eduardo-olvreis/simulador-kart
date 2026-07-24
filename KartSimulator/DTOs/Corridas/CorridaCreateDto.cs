8f2c68bb4fd65f929d8e27372b1b5c79a1c6256c
Eduardo <eduardo.olvreis@gmail.com>
2026-07-23 21:01:17 -0300

    feat: CorridaCreateDto concluida

---
+++ KartSimulator/DTOs/Corridas/CorridaCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KartSimulator.DTOs.Corridas
+{
+    public class CorridaCreateDto
+    {
+        [Required(ErrorMessage = "Campo 'Nome da Pista' não pode ser nulo.")]
+        [MaxLength(60, ErrorMessage = "Campo 'Nome da Pista' não pode ter mais de 60 caracteres.")]
+        public string NomePista { get; set; }
+
+        [Range(10, 50, ErrorMessage = "Campo 'Numero de Voltas' deve estar entre o intervalo de 10 a 50.")]
+        public int NumeroVoltas { get; set; }
+    }
+}
