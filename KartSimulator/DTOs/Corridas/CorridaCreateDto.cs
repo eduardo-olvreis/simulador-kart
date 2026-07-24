5616d29dcd159cf6bf4025e9b9852c70c70f0700
Eduardo <eduardo.olvreis@gmail.com>
2026-07-23 21:11:16 -0300

    feat: CorridaCreateDto adição do Pilotos Ids

--- KartSimulator/DTOs/Corridas/CorridaCreateDto.cs
+++ KartSimulator/DTOs/Corridas/CorridaCreateDto.cs
@@ -10,5 +10,10 @@ namespace KartSimulator.DTOs.Corridas
 
         [Range(10, 50, ErrorMessage = "Campo 'Numero de Voltas' deve estar entre o intervalo de 10 a 50.")]
         public int NumeroVoltas { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Pilotos Ids' não pode ser nulo.")]
+        [MinLength(2, ErrorMessage = "Campo 'Pilotos Ids' tem que ser no minimo 2")]
+        [MaxLength(12, ErrorMessage = "Campo 'Pilotos Ids' tem que ser no máximo 12")]
+        public List<int> PilotosIds { get; set; } = new();
     }
 }
