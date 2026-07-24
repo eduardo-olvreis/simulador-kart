a6c8288b0e54f0f8775515a11846791042204a65
Eduardo <eduardo.olvreis@gmail.com>
2026-07-23 21:06:13 -0300

    feat: CorridaResponseDto concluida

---
+++ KartSimulator/DTOs/Corridas/CorridaResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using KartSimulator.Entities;
+
+namespace KartSimulator.DTOs.Corridas
+{
+    public class CorridaResponseDto
+    {
+        public int Id { get; set; }
+        public string NomePista { get; set; }
+        public int NumeroVoltas { get; set; }
+        public DateTime DataCorrida { get; set; }
+        public List<ResultadoCorrida> Resultados { get; set; } = new();
+    }
+}
